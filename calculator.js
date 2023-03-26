bb250c556201d455b9b6e173066f12e489d2dc20
Brian <linbrian24@gmail.com>
2023-03-25 23:53:15 -0400

    Complete operate function

--- calculator.js
+++ calculator.js
@@ -18,11 +18,22 @@ const divide = (num1, num2) => {
     return num1 / num2;
 }
 
-const operate = (num1, operator, num2) => {
+const operate = (operator, num1, num2) => {
+    if (operator === "+") {
+        return add(num1, num2);
+    }
 
+    else if (operator === "-") {
+        return subtract(num1, num2);
+    }
+
+    else if (operator === "*") {
+        return multiply(num1, num2);
+    }
+
+    else if (operator === "/") {
+        return divide(num1, num2);
+    }
 }
 
-console.log(add(1,1));
-console.log(subtract(1,1));
-console.log(multiply(2,3));
-console.log(divide(4, 2));
+console.log(operate("+", 2, 1));
