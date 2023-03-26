9c0f64b68233753c4ca284da7fd96b11af76c247
Brian <linbrian24@gmail.com>
2023-03-25 23:48:31 -0400

    Create basic math operations in JS

--- calculator.js
+++ calculator.js
@@ -2,3 +2,27 @@ let num1;
 let operator;
 let num2;
 
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+const subtract = (num1, num2) => {
+    return num1 - num2;
+}
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+const divide = (num1, num2) => {
+    return num1 / num2;
+}
+
+const operate = (num1, operator, num2) => {
+
+}
+
+console.log(add(1,1));
+console.log(subtract(1,1));
+console.log(multiply(2,3));
+console.log(divide(4, 2));
