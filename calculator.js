d4b13101946794e278586d66cfb3b9d93097d224
Brian <linbrian24@gmail.com>
2023-03-27 11:58:28 -0400

    Update display and add function

--- calculator.js
+++ calculator.js
@@ -5,7 +5,7 @@ let num2;
 let display = "";
 
 const add = (num1, num2) => {
-    return num1 + num2;
+    return parseInt(num1) + parseInt(num2);
 }
 
 const subtract = (num1, num2) => {
@@ -29,7 +29,7 @@ const operate = (operator, num1, num2) => {
         return subtract(num1, num2);
     }
 
-    else if (operator === "*") {
+    else if (operator === "x") {
         return multiply(num1, num2);
     }
 
@@ -43,7 +43,10 @@ const changeDisplay = (value) => {
     calcDisplay.textContent = display;
 }
 
-const numKeys = document.querySelectorAll("button");
+const numKeys = document.querySelectorAll(".digits");
+const operators = document.querySelectorAll(".operator");
+const enterKey = document.querySelector(".calculator__enter");
+const clear = document.querySelector(".calculator__clear");
 const calcDisplay = document.querySelector(".calculator__display");
 
 numKeys.forEach((key) => {
@@ -51,4 +54,17 @@ numKeys.forEach((key) => {
     key.addEventListener("click", () => {
         changeDisplay(key.textContent);
     });
+});
+
+operators.forEach((key) => {
+    key.addEventListener("click", () => {
+        operator = key.textContent.trim();
+        changeDisplay(key.textContent);
+    });
+});
+
+enterKey.addEventListener("click", () => {
+    const expression = display.split(operator);
+    display = "";
+    calcDisplay.textContent = operate(operator, expression[0], expression[1]);
 });
